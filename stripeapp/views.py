9373aa494eb6e40f4845934f21b4ed718ffeb851
Hamza Saleem <55046070+haamzaasaleem@users.noreply.github.com>
2022-11-14 19:35:25 +0500

    Webhook View Updated

--- stripeapp/views.py
+++ stripeapp/views.py
@@ -18,6 +18,8 @@ STRIPE_SECRET_KEY = "sk_test_51KoniGKcrwcO2WsbAgrJCNGb4q6cuKbHXyXBmyZObyFs8oJffr
 STRIPE_ENDPOINT_SECRET="whsec_8563e72f8cd66e0bd9d73ccda5fd0924dec3b605e3072593c54cde3295839e3f"
 
 endpoint_secret=''
+
+
 @csrf_exempt
 def custom_webhook(request):
     print('webhook!')
@@ -26,7 +28,7 @@ def custom_webhook(request):
     body = json.loads(payload)
     print("payload:", body)
 
-    if endpoint_secret:
+    if STRIPE_ENDPOINT_SECRET:
         # print(request.headers['Stripe-Signature'])
         sig_header = request.headers.get('stripe-signature')
         try:
@@ -39,23 +41,11 @@ def custom_webhook(request):
             return HttpResponse(status=400)
         if event['type'] == 'checkout.session.completed':
             session = event['data']['object']
-            print("session:", session['customer'])
-            print("session:", session)
-
-            # using the timestamp() function to convert datetime into epoch time
             epoch_time = datetime.datetime.now().timestamp()
             date_today = datetime.datetime.today().date()
             end_data = datetime.datetime.today().date() + datetime.timedelta(days=14)
-
-            # printing the values
-            print("Converted epoch time:", epoch_time)
-            print("Today::", date_today)
-            print("end_data::", end_data)
-            docarray = []
             # cus_MncMBUmB7CozRw
-            collection = database.collection('subscriptions').where('customerId', '==', 'cus_MNmFEiUZf00IOP').get()
-            print("user history matched records", collection)
-            # if
+            collection = database.collection('subscriptions').where('customerId', '==', session['customer']).get()
             for doc in collection:
                 print("doc_id:", doc.id)
                 currentTierData = {
@@ -67,7 +57,6 @@ def custom_webhook(request):
                     'interval': 'Yearly',
                     'startDate': str(date_today),
                     'tier': "Done by Us!"
-
                 }
                 trial_data = {
                     'active': True,
@@ -79,7 +68,6 @@ def custom_webhook(request):
                     }
                 }
                 if doc.to_dict()['currentTier']:
-                    # doc.to_dict()['currentTier'].document(doc.id).update({'active':True})
                     database.collection('subscriptions').document(doc.id).update(
                         {"currentTier": currentTierData,
                          'trial': trial_data
